10554b328d2fd1f97406fe3a6976efc4f7d1294d
Florian <florian.fort0@gmail.com>
2020-05-18 19:25:15 +0200

    all css

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -27,28 +27,28 @@ class Slider {
 		this.domSlider.innerHTML = `
 			<figure id="figure0">
 				<img src="images/img_lyon.jpg">
-				<figcaption>Louer un vélo pour vous déplacer rapidement dans Lyon !</figcaption>
+				<figcaption>1Louer un vélo pour vous déplacer rapidement dans Lyon !</figcaption>
 			</figure>
 			<figure id="figure1">
 				<img src="images/img3_velo_ConvertImage.jpg">
-				<figcaption>Regardez la station la plus proche de votre position sur la map. Cliquez sur la station, vous aurez son adresse, et le nombre de vélos disponibles.</figcaption>
+				<figcaption>2Regardez la station la plus proche de votre position sur la map. Cliquez sur la station, vous aurez son adresse, et le nombre de vélos disponibles.</figcaption>
 			</figure>
 			<figure id="figure2">
 				<img src="images/img_foret.jpg">
-				<figcaption>Il ne vous reste plus qu'à réserver le vélo grâce au formulaire puis à aller le chercher dans les 20 minutes qui suivent. Si vous dépassez ce temps, il faudra refaire une réservation du vélo.</figcaption>
+				<figcaption>3Il ne vous reste plus qu'à réserver le vélo grâce au formulaire puis à aller le chercher dans les 20 minutes qui suivent. Si vous dépassez ce temps, il faudra refaire une réservation du vélo.</figcaption>
 			</figure>
 			<figure id="figure3">
 				<img src="images/jardin.jpg">
-				<figcaption>Pour terminer, déposez le vélo dans une station où des places sont disponibles.</figcaption>
+				<figcaption>4Pour terminer, déposez le vélo dans une station où des places sont disponibles.</figcaption>
 			</figure>
 			`;
 	}
 
 	btnTemplate(){
 	    this.dom.innerHTML = `
-	      	<button class="arrowPrev"  onClick="webBike.${this.name}.changeSlide('+')" > <i class="fa fa-arrow-left" aria-hidden="true">  </i></button>
+	      	<button class="arrowPrev"  onClick="webBike.${this.name}.changeSlide('-')" > <i class="fa fa-arrow-left" aria-hidden="true">  </i></button>
 			<button class="playBtn" id="playPause"  onClick="webBike.${this.name}.playPause()" >  		<i class="fa fa-pause" aria-hidden="true">       </i></button>
-			<button class="arrowNext"  onClick="webBike.${this.name}.changeSlide('-')" > <i class="fa fa-arrow-right" aria-hidden="true"> </i></button>
+			<button class="arrowNext"  onClick="webBike.${this.name}.changeSlide('+')" > <i class="fa fa-arrow-right" aria-hidden="true"> </i></button>
 	    `;
   	}
 
@@ -79,11 +79,6 @@ class Slider {
 		this.tempo = setTimeout(this.changeSlide.bind(this), this.duree, "+");
 	}
 
-	play() {
-		if (this.ecoule === null) {
-	    }
-	}
-
 	pause() {
 		document.getElementById(`figure${this.idFigure}`).style.animationPlayState = "running"; 
 		this.tempo = setTimeout(this.changeSlide, this.duree - this.ecoule, "+");
@@ -92,16 +87,20 @@ class Slider {
 
 	playPause(){
 		if (this.playPauseBtn === undefined) this.playPauseBtn = document.getElementById("playPause");
+
 		if (this.ecoule === null){
-	    clearTimeout(this.tempo);
-	    document.getElementById(`figure${this.idFigure}`).style.animationPlayState = "paused";
-	    this.ecoule = Date.now()-this.start;
-	    this.playPauseBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-	    return;
+		    clearTimeout(this.tempo);
+
+		    document.getElementById(`figure${this.idFigure}`).style.animationPlayState = "paused";
+		    this.ecoule = Date.now()-this.start;
+		    this.playPauseBtn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+
+		    return;
 		}
+
 		document.getElementById(`figure${this.idFigure}`).style.animationPlayState = "running"; 
 		this.tempo = setTimeout(this.changeSlide, this.duree - this.ecoule, "+");
 		this.ecoule = null;
-	  this.playPauseBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+	  	this.playPauseBtn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
 	}
 }
